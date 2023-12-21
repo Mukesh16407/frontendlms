53bc75784e44bded307385b7addffcd92cfcc2ac
Mukesh16407 <mukesh.16407@gmail.com>
2023-12-21 19:41:07 +0530

    header and home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css';
 import { Home } from './pages/Home';
+import Header from './components/Layout/Header/Header';
 
 function App() {
   return (
     <BrowserRouter>
+     <Header/>
       <Routes>
         <Route path='/'element={<Home/>}/>
       </Routes>
